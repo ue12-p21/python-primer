50b1f726cf90183b17c4bcc193a102ade683d426
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2021-09-08 11:13:39 +0200

    exos optionnels

---
+++ notebooks/2-99-exos-optionnels.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-hidden,-heading_collapsed,-run_control,-trusted
+#     notebook_metadata_filter: all, -jupytext.text_representation.jupytext_version,
+#       -jupytext.text_representation.format_version, -language_info.version, -language_info.codemirror_mode.version,
+#       -language_info.codemirror_mode, -language_info.file_extension, -language_info.mimetype,
+#       -toc
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+#   language_info:
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#   nbhosting:
+#     title: exos optionnels
+# ---
+
+# %% [markdown]
+# # exercices optionnels
+#
+# pour les rapides qui ont déjà terminé
+#
+# à faire directement sur la plateforme codewars
+
+# %% [markdown]
+# ## facile (8 kyu)
+#
+# * Cascading Subsets  
+#   https://www.codewars.com/kata/545af3d185166a3dec001190/train/python
+# * What is between ?  
+#   https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/python
+# * Beginner Series #4 Cockroach  
+#   https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/python
+
+# %% [markdown]
+# ## moyen (7 kyu)
+#
+# * Integer Difference  
+#   https://www.codewars.com/kata/57741d8f10a0a66915000001/train/python
+
+# %% [markdown]
+# ## un peu plus dur (6 kyu)
+#
+# * Bishop movement checker  
+#   https://www.codewars.com/kata/6135e4f40cffda0007ce356b/train/python
+
+# %% [markdown]
+# Comme vous le voyez sur codewars il y a des centaines de problèmes de programmation (et pas que en Python d'ailleurs), faites votre choix...
